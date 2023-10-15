a06bef43ff001f968f289bc71f055553b8d4445d
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2023-10-15 19:47:30 +0200

    blub

--- CreatType/main.cpp
+++ CreatType/main.cpp
@@ -1,9 +1,15 @@
+/*Shows how to use an Own Type
+Unary arithmetic operators
+*/
+
 #include <iostream>
 #include "vec2.h"
 
+//Creating own Typ
 Vec2::Vec2(float x, float y)
     : x_(x), y_(y)
     {}
+
 
 void Vec2::set_x(float x) 
 {
@@ -20,14 +26,17 @@ int main() {
     Vec2 b = a;
     //Manipulating B
     b.set_x(3);
-    Vec2 c();
-    //Auto typ
-    //auto c = -b;
-    //auto d = (a+c)*3;
+    //Vec2 c();
+
+    //Auto typ with unary operator  test
+    auto c = -b;
+
+    auto d = (a+c)*3;
 
     std::cout << a.x() << " " << a.y() << std::endl;
     std::cout << b.x() << " " << b.y() << std::endl;
-    //std::cout << c.x() << " " << c.y() << std::endl;
-    //std::cout << d.x() << " " << d.y() << std::endl;
+
+    std::cout << c.x() << " " << c.y() << std::endl;
+    std::cout << d.x() << " " << d.y() << std::endl;
     return 0;   
 }
--- CreatType/vec2.h
+++ CreatType/vec2.h
@@ -10,6 +10,22 @@ public:
     void set_x(float x);
     void set_y(float y);
 
+    // Unary operator method
+    Vec2 Vec2::operator-() const 
+    {
+        return Vec2(-x_, -y_);
+    }
+    // Binary overloading operator method
+    //rhs = right hand side convention
+    Vec2 Vec2::operator+( Vec2 rhs) const
+    {
+        return Vec2(x()+rhs.x(), y()+rhs.y());
+    }
+    Vec2 Vec2::operator*(float rhs) const
+    {
+        return Vec2(x()*rhs, y()*rhs);
+    }
+
 
     private:
         float x_, y_;
