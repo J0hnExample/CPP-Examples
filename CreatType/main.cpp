9778b2d56faa99394f341ee6ae1ce90013efb35e
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2023-10-16 21:26:46 +0200

    0:=

--- CreatType/main.cpp
+++ CreatType/main.cpp
@@ -5,6 +5,7 @@ try to modify Call By Value
 
 #include <iostream>
 #include "vec2.h"
+#include <assert.h>
 
 //Creating own Typ
 Vec2::Vec2(float x, float y)
@@ -20,17 +21,35 @@ void Vec2::set_y(float y)
 {
     y_ = y;
 }
+
+//Shows the differenz in call by reference/Value
 //Call By Value
+//v is a copy of a
 void try_to_modify(Vec2 v)
 {
+    //sets v.x to 3.14 
     v.set_x(3.14f);
+}
+//Call by reference
+void try_to_modify_ref(Vec2& v)
+{
+    //sets v.x to 3.14 
+    v.set_x(3.14f);
+}
+//Constant ref
+void try_to_modify_const_ref(const Vec2& v)
+{
+    //sets v.x to 3.14 
+   //Not allowed v.set_x(3.14f);
 }
 int main() {
     Vec2 a(2,2);
     //Copy 
     Vec2 b = a;
-    //Try to Modify
-    try_to_modify(a);
+    //Try to Modify (no harm to a)
+    try_to_modify_ref(a);
+    //Testing assert
+    assert(a.x() == 3.14f);
     //Manipulating B
     b.set_x(3);
     //Vec2 c();
--- CreatType/vec2.h
+++ CreatType/vec2.h
@@ -19,13 +19,38 @@ public:
     }
     // Binary overloading operator method
     //rhs = right hand side convention
-    Vec2 Vec2::operator+( Vec2 rhs) const
+    //outdated
+  /*   Vec2 Vec2::operator+( Vec2 rhs) const
     {
         return Vec2(x()+rhs.x(), y()+rhs.y());
+    } */
+    //New Fancy +operator with use of +=
+    Vec2 Vec2::operator+(const Vec2& rhs)
+    {
+        Vec2 temp(*this); //copy constructor
+        temp += rhs; //operator+=() see above
+        return temp;    
     }
+    //+ vs += / preventing redundant code
+    Vec2& Vec2::operator+=(Vec2 rhs)
+    {
+        x_ += rhs.x();
+        y_ += rhs.y();
+        return *this; // return object itself
+    }
+    //*
     Vec2 Vec2::operator*(float rhs) const
     {
         return Vec2(x()*rhs, y()*rhs);
+    }
+    //More Operators
+    //Gives back a Vec2 reference
+    Vec2& Vec2::operator=(const Vec2& rhs)
+    {
+        x_ = rhs.x_;
+        y_ = rhs.y_;
+
+        return *this; // return object itself
     }
 
 
