9675b4dd849b2ff3fd3ddb9a6d2a3aa7f789fdd0
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2023-10-17 10:37:43 +0200

    a1-1

--- A1-1/main.cpp
+++ A1-1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "test11.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
---
+++ A1-1/vec3f.cpp
@@ -0,0 +1,6 @@
+
+class Vec3f{
+    public:
+        //std::array with 3 floats
+        
+}
