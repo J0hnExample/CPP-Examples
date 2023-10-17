0fdcaee09e04a8e2337650bf1905e0a9ef7593aa
SaschaAthmer <33932092+J0hnExample@users.noreply.github.com>
2023-10-17 11:19:40 +0200

    subscript operator

--- A1-1/main.cpp
+++ A1-1/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "test11.h"
+#include "vec3f.cpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Vec3f a;
+    
+    size_t size = a.getSize();
+
+    auto f = a[1];
+
+    std::cout << "Array Size: "<< size  << std::endl;
     return 0;   
 }
--- A1-1/vec3f.cpp
+++ A1-1/vec3f.cpp
@@ -1,6 +1,28 @@
+#include <array>
+#include <vector>
 
-class Vec3f{
-    public:
-        //std::array with 3 floats
-        
-}
+class Vec3f {
+public:
+    std::array<std::vector<float>, 3> arr;
+
+    //Default construktor with d. values
+    Vec3f(float a=1, float b=1, float c=1) {
+        arr[0].push_back(a);
+        arr[1].push_back(b);
+        arr[2].push_back(c);
+    }
+
+    
+    Vec3f(std::array<std::vector<float>, 3> arr) : arr(arr) {
+        // 
+        this->arr[0].push_back(1);
+        this->arr[1].push_back(1);
+        this->arr[2].push_back(1);
+    }
+    size_t getSize(){
+        return sizeof(arr)/sizeof(arr[0]);
+    }
+
+    //Operators
+    Vec3f Vec3f::operator[]
+};
